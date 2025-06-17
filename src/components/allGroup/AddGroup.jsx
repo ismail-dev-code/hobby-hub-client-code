29ff58afa936774bbb6cf770d3353c9d2726c137
ismail-dev-code <hm.ismail772@gmail.com>
2025-06-17 23:26:11 +0600

    feat: make the description field text area

--- src/components/allGroup/AddGroup.jsx
+++ src/components/allGroup/AddGroup.jsx
@@ -33,128 +33,129 @@ const AddGroup = () => {
 
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>HobbyHub | Create Group</title>
       </Helmet>
-    <div className="px-4 sm:px-6 md:px-12 lg:px-24 py-12">
-      <div className="text-center px-2 sm:px-4 md:px-8 lg:px-12 space-y-4">
-        <h1 className="text-2xl sm:text-5xl md:text-4xl font-bold">
-          Create Group
-        </h1>
-        <p className="text-base sm:text-lg text-gray-600 mt-2 sm:mt-4">
-          Use the form below to add a new group to the collection. Fill in the
-          details like Group Name, Hobby Category, Description, Meeting
-          Location, Max Members, End Date and image URL.
-        </p>
-      </div>
-
-      <form onSubmit={handleAddGroup} className="mt-10">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-            <label className="label">Group Name</label>
-            <input
-              required
-              type="text"
-              name="name"
-              className="input w-full"
-              placeholder="Enter your group name"
-            />
-          </fieldset>
-          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-            <label className="label">Hobby Categories</label>
-            <select name="category" className="select w-full">
-              <option>Drawing & Painting</option>
-              <option>Photography</option>
-              <option>Video Gaming</option>
-              <option>Fishing</option>
-              <option>Running</option>
-              <option>Cooking</option>
-              <option>Reading</option>
-              <option>Writing </option>
-            </select>
-          </fieldset>
-          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-            <label className="label">Description</label>
-            <input
-              required
-              type="text"
-              name="description"
-              className="input w-full"
-              placeholder="Enter Description"
-            />
-          </fieldset>
-          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-            <label className="label">Meeting Location</label>
-            <input
-              required
-              type="text"
-              name="location"
-              className="input w-full"
-              placeholder="Enter meeting location"
-            />
-          </fieldset>
-          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-            <label className="label">Max Members</label>
-            <input
-              required
-              type="number"
-              name="members"
-              className="input w-full"
-              placeholder="Enter max members"
-            />
-          </fieldset>
-          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
-            <label className="label">End Date</label>
-            <input
-              required
-              type="date"
-              name="date"
-              className="input w-full"
-              placeholder="Enter start date"
-            />
-          </fieldset>
+      <div className="px-4 sm:px-6 md:px-12 lg:px-24 py-12">
+        <div className="text-center px-2 sm:px-4 md:px-8 lg:px-12 space-y-4">
+          <h1 className="text-2xl sm:text-5xl md:text-4xl font-bold">
+            Create Group
+          </h1>
+          <p className="text-base sm:text-lg text-gray-600 mt-2 sm:mt-4">
+            Use the form below to add a new group to the collection. Fill in the
+            details like Group Name, Hobby Category, Description, Meeting
+            Location, Max Members, End Date and image URL.
+          </p>
         </div>
 
-        <fieldset className="fieldset my-6 bg-base-200 border-base-300 rounded-box border p-4">
-          <label className="label">Photo</label>
-          <input
-            required
-            type="url"
-            name="photo"
-            className="input w-full"
-            placeholder="Photo URL"
-          />
-        </fieldset>
-        <fieldset className="fieldset my-6 bg-base-200 border-base-300 rounded-box border p-4">
-          <label className="label">User Name</label>
-          <input
-            required
-            value={user?.displayName}
-            type="text"
-            name="userName"
-            className="input w-full"
-            placeholder="Dynamic user name"
-          />
-        </fieldset>
-        <fieldset className="fieldset my-6 bg-base-200 border-base-300 rounded-box border p-4">
-          <label className="label">User Email</label>
-          <input
-            required
-            value={user?.email}
-            type="email"
-            name="userEmail"
-            className="input w-full"
-            placeholder="Dynamic user email"
-          />
-        </fieldset>
+        <form onSubmit={handleAddGroup} className="mt-10">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+              <label className="label">Group Name</label>
+              <input
+                required
+                type="text"
+                name="name"
+                className="input w-full"
+                placeholder="Enter your group name"
+              />
+            </fieldset>
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+              <label className="label">Hobby Categories</label>
+              <select name="category" className="select w-full">
+                <option>Drawing & Painting</option>
+                <option>Photography</option>
+                <option>Video Gaming</option>
+                <option>Fishing</option>
+                <option>Running</option>
+                <option>Cooking</option>
+                <option>Reading</option>
+                <option>Writing </option>
+              </select>
+            </fieldset>
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+              <label className="label">Description</label>
+              <textarea
+                required
+                name="description"
+                className="textarea w-full"
+                placeholder="Enter Description"
+                rows={4}
+              ></textarea>
+            </fieldset>
 
-        <input
-          type="submit"
-          className="btn bg-violet-500 hover:bg-violet-600 text-white w-full mt-4"
-          value="Create Group"
-        />
-      </form>
-    </div>
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+              <label className="label">Meeting Location</label>
+              <input
+                required
+                type="text"
+                name="location"
+                className="input w-full"
+                placeholder="Enter meeting location"
+              />
+            </fieldset>
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+              <label className="label">Max Members</label>
+              <input
+                required
+                type="number"
+                name="members"
+                className="input w-full"
+                placeholder="Enter max members"
+              />
+            </fieldset>
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+              <label className="label">End Date</label>
+              <input
+                required
+                type="date"
+                name="date"
+                className="input w-full"
+                placeholder="Enter start date"
+              />
+            </fieldset>
+          </div>
+
+          <fieldset className="fieldset my-6 bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Photo</label>
+            <input
+              required
+              type="url"
+              name="photo"
+              className="input w-full"
+              placeholder="Photo URL"
+            />
+          </fieldset>
+          <fieldset className="fieldset my-6 bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">User Name</label>
+            <input
+              required
+              value={user?.displayName}
+              type="text"
+              name="userName"
+              className="input w-full"
+              placeholder="Dynamic user name"
+            />
+          </fieldset>
+          <fieldset className="fieldset my-6 bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">User Email</label>
+            <input
+              required
+              value={user?.email}
+              type="email"
+              name="userEmail"
+              className="input w-full"
+              placeholder="Dynamic user email"
+            />
+          </fieldset>
+
+          <input
+            type="submit"
+            className="btn bg-violet-500 hover:bg-violet-600 text-white w-full mt-4"
+            value="Create Group"
+          />
+        </form>
+      </div>
     </>
   );
 };
